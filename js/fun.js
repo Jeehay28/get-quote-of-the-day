7ebf5b17b18359e959c84c18056998fcdfa29425
jeehay <sophiepark528@gmail.com>
2023-09-25 00:12:02 +0900

    Fixing token problem

--- js/fun.js
+++ js/fun.js
@@ -3,11 +3,10 @@ window.onclick = e => {
     clicked.classList.add("active");
 }
 
-// const url = "https://dummyjson.com/quotes"
 // https://api.github.com/repos/:owner/:repo/contents/:path
 
 const url = "https://api.github.com/repos/Jeehay28/get-quote-of-the-day/contents/src/msg.json";
-const personalAccessToken = "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"
+const personalAccessToken = process.env.PERSONALACCESSTOKEN; // Access the environment variable
 
 const headers = new Headers({
     'Authorization': `token ${personalAccessToken}`,
@@ -19,42 +18,6 @@ const requestOptions = {
  };
 
 const before = document.querySelector("#before");
-
-
-// fetch(url, requestOptions)
-//     .then(response => response.json())
-//     .then(obj => {
-//         console.log(typeof obj) // object
-//         console.log(obj);
-//         console.log(Object.keys(obj))
-//         console.log(obj.quotes)
-//         before.innerHTML =
-//             `<h2>${obj.quotes[num].quote}</h2>
-//              <h2 style="color : grey">${obj.quotes[num].author}</h2>`
-//     })
-
-// fetch(url, requestOptions)
-// .then(response => response.json())
-// .then(data => {
-//     // Decode the Base64 content
-//     const content = atob(data.content);
-//     const obj = JSON.parse(content);
-    
-//     console.log(obj.quotes.length);
-//     const num = Math.floor(Math.random() * obj.quotes.length);
-//     console.log(num);
-
-//     if (obj.quotes && obj.quotes[num]) {
-//         // Check if obj.quotes[num] exists before accessing its properties
-//         before.innerHTML =
-//             `<h2>${obj.quotes[num].quote}</h2>
-//              <h2 style="color : grey">${obj.quotes[num].author}</h2>`;
-//     } else {
-//         console.error('Invalid index or data structure.');
-//     }
-// })
-// .catch(error => console.error('Error:', error));
-
 
 fetch(url, requestOptions)
   .then(response => {
