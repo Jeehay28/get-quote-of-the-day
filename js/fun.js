2d6ba116d630ff52efb3bebd02bfc00bb6240339
jeehay <sophiepark528@gmail.com>
2023-09-24 23:27:42 +0900

    Fixing

--- js/fun.js
+++ js/fun.js
@@ -7,16 +7,16 @@ window.onclick = e => {
 // https://api.github.com/repos/:owner/:repo/contents/:path
 
 const url = "https://api.github.com/repos/Jeehay28/get-quote-of-the-day/contents/src/msg.json";
-const personalAccessToken = "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"
+const personalAccessToken = "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"
 
 const headers = new Headers({
     'Authorization': `token ${personalAccessToken}`,
   });
-  
-  const requestOptions = {
+
+const requestOptions = {
     method: 'GET',
     headers,
-  };
+ };
 
 const before = document.querySelector("#before");
 
@@ -57,16 +57,40 @@ const before = document.querySelector("#before");
 
 
 fetch(url, requestOptions)
-    .then(response => response.json())
-    .then(obj => {
-        if (obj.quotes && obj.quotes.length > 0) {
-            const num = Math.floor(Math.random() * obj.quotes.length);
-            const quote = obj.quotes[num];
-            before.innerHTML =
-                `<h2>${quote.quote}</h2>
-                 <h2 style="color: grey">${quote.author}</h2>`;
-        } else {
-            console.error('Invalid JSON data structure or no quotes found.');
-        }
-    })
-    .catch(error => console.error('Error:', error));
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    console.log(response)
+    return response.json(); // Parse the response as JSON
+
+  })
+  .then(data => {
+    try {
+      // Decode the Base64 content
+      const content = atob(data.content);
+
+      // Now you can work with the decoded content as a string
+      console.log(content);
+
+      // Parse the decoded content as JSON
+      const obj = JSON.parse(content);
+
+      console.log(obj);
+
+      // Now you can work with the JSON object
+      if (obj.quotes && obj.quotes.length > 0) {
+        const num = Math.floor(Math.random() * obj.quotes.length);
+        console.log(num);
+
+        before.innerHTML =
+          `<h2>${obj.quotes[num].quote}</h2>
+           <h2 style="color : grey">${obj.quotes[num].author}</h2>`;
+      } else {
+        console.error('Invalid index or data structure.');
+      }
+    } catch (error) {
+      console.error('Error decoding or parsing JSON:', error);
+    }
+  })
+  .catch(error => console.error('Error:', error));
